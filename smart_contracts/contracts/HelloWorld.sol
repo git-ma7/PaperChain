aaa6afe6655ae58e4bf92f14b1f83f1edfdc0b6c
anshuljani <anshuljani13@gmail.com>
2025-10-05 23:09:50 +0530

    testing

--- smart_contracts/contracts/HelloWorld.sol
+++ smart_contracts/contracts/HelloWorld.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.28;
 
 contract HelloWorld {
-    string public greet = "Hello, bitches";
+    string public greet = "Hello, People!";
 
     function setGreeting(string memory _greeting) public{
         greet = _greeting;
